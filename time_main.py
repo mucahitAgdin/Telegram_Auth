4aea65676d45e98da5e4aa24dbfc5e2053267ea9
Mücahit <81189443+mucahitAgdin@users.noreply.github.com>
2022-07-25 12:22:18 +0300

    new

--- time_main.py
+++ time_main.py
@@ -4,23 +4,29 @@ import time
 
 def countdown(t):
     while t:
-        mins, secs = divmod(t, 60)
+        mins, secs = divmod(t, 100)
         timer = '{:02d}:{:02d}'.format(mins, secs)
         print(timer, end='\r')
         time.sleep(1)
         t -= 1
-        user_list = file_transactions.read_json("users")
-        delete_user_list = file_transactions.read_json("deleted_user_list")
-        deleting_user_list = []
-        for x in user_list:
-            user_list[x]['days_left'] -= 1
-            if(user_list[x]['days_left'] == 0):
-                deleting_user_list.append(x)
-        for i in deleting_user_list:
-            del user_list[i]
-            delete_user_list.append(i)
-        file_transactions.convert_to_json(user_list, "users")
-        file_transactions.convert_to_json(
-            delete_user_list, "deleted_user_list")
-			
-countdown(5)
+
+def deleting_users():
+    user_list = file_transactions.read_json("users")
+    delete_user_list = file_transactions.read_json("deleted_user_list")
+    deleting_user_list = []
+    for x in user_list:
+        user_list[x]['days_left'] -= 1
+        if(user_list[x]['days_left'] == 0):
+            deleting_user_list.append(x)
+    for i in deleting_user_list:
+        del user_list[i]
+        delete_user_list.append(i)
+    file_transactions.convert_to_json(user_list, "users")
+    file_transactions.convert_to_json(
+        delete_user_list, "deleted_user_list")
+
+while True:
+    countdown(5)
+    deleting_users()
+
+
